45875d49cf819a7711867373c495b6eb80fb12b6
Jake Smiley <jake.alan.smiley@gmail.com>
2020-08-15 21:44:32 -0600

    random attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,51 +37,51 @@ var fightOrSkip = function() {
 
 // create fight function
 var fight = function(enemy) {
+    var isPlayerTurn = true;
+
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+
     // repeat and execute as long as the enemy robot is alive 
     while(enemy.health > 0 && playerInfo.health > 0) {
-        if (fightOrSkip()) {
-            break;
+        if (isPlayerTurn) {
+            //ask fight or skip
+            if (fightOrSkip()) {
+                //leave fight by breaking loop if true
+                break;
+            }
+
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " has " + enemy.health + " health remaining.");
+
+            if (enemy.health <=0) {
+                window.alert(enemy.name + " has died!");
+
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.")
+            }
+        } else {
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name + " has attacked " + playerInfo.name + " . " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
-  
-    // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-    enemy.health = Math.max(0, enemy.health - damage);
-
-    console.log(playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.');
-  
-      // check enemy's health
-    if (enemy.health <= 0) {
-        window.alert(enemy.name + ' has died!');
-  
-        // award player money for winning
-        playerInfo.money = playerInfo.money + 20;
-  
-        // leave while() loop since enemy is dead
-        break;
-
-    } else {
-        window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
-    }
-  
-    // remove players's health by subtracting the amount set in the enemy.attack variable
-    var damage = randomNumber(enemy.attack - 3, enemy.attack);
-    playerInfo.health = Math.max(0, playerInfo.health - damage);
-    
-    console.log(
-        enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
-    );
-  
-        // check player's health
-    if (playerInfo.health <= 0) {
-        window.alert(playerInfo.name + ' has died!');
-        // leave while() loop if player is dead
-        break;
-    } else {
-        window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
-    }
+        //switch turn order
+        isPlayerTurn = !isPlayerTurn;
     }
 };
-
 
 // start game function
 var startGame = function() {
