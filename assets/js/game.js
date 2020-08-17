f452ab206b4975f42d3beb2c6473112c7ddad561
Jake Smiley <jake.alan.smiley@gmail.com>
2020-08-16 18:59:50 -0600

    high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -118,13 +118,24 @@ var startGame = function() {
 
 // end game function
 var endGame = function() {
-    // win
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the gauntlet! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("You've lost your robot in battle!");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
     }
-    
+    // if player have more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+  
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } 
+    else {
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }    
+
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
